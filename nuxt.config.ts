26f37a3e95e901a54dc6f4bde29435ce71a4c3e9
Nurul Nabila Natasya <156649882+nrl-nabilanatasya@users.noreply.github.com>
2025-01-12 15:06:26 +0800

    feat(config): add SEO meta tags
    - Added SEO meta tags for title, description, and social media sharing in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,7 +20,21 @@ export default defineNuxtConfig({
 
   app: {
     head: {
-      title: "Nuxt Math Quiz — Bridge International Asia Sdn. Bhd",
+      title: process.env.META_TITLE,
+      meta: [
+        { name: 'description', content: process.env.META_DESCRIPTION },
+        { property: 'og:title', content: process.env.META_TITLE },
+        { property: 'og:description', content: process.env.META_DESCRIPTION },
+        { property: 'og:image', content: process.env.META_IMAGE },
+        { property: 'og:url', content: process.env.META_URL },
+        { name: 'twitter:card', content: process.env.META_TWITTER_CARD },
+        { name: 'twitter:title', content: process.env.META_TITLE },
+        { name: 'twitter:description', content: process.env.META_DESCRIPTION },
+        { name: 'twitter:image', content: process.env.META_IMAGE }
+      ],
+      link: [
+        { rel: "icon", type: "image/png", href: process.env.META_IMAGE },
+      ],
       htmlAttrs: {
         lang: 'en',
       }
